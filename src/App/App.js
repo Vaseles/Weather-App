068f18be20426e9108f97a5cc20d4424f2c92418
Vaseles <82625479+Vaseles@users.noreply.github.com>
2023-02-21 19:55:31 +0600

    Added Search

--- src/App/App.js
+++ src/App/App.js
@@ -3,20 +3,24 @@ import axios from 'axios';
 import styles from './App.module.scss';
 import Mountine from './../assets/images/image7.png'
 import Sunset from './../assets/images/image8.png'
+import {FaSearch} from 'react-icons/fa';
+
 
 const App = () => {
   const dateNow = new Date();
   const [data, setData] = useState()
   const fullTime = `${dateNow.getHours()}:${dateNow.getMinutes()} ${dateNow.getDate()}.${dateNow.getMonth()}.${dateNow.getFullYear()}`
+  const [search, setSearch] = useState('Pavlodar');
+  const [error, setError] = useState('')
 
   useEffect (() => {
-    getData();
+    getData(search);
   }, [])
 
-  const getData = async () => {
+  const getData = async (search) => {
     axios.get('https://api.openweathermap.org/data/2.5/weather', {
   params: {
-      q: 'Pavlodar,kz',
+      q: `${search},kz`,
       units: 'metric',
       appid: 'a600c78412029f95fe5046de660f57e2'
     }
@@ -24,16 +28,28 @@ const App = () => {
   .then(res => {
     console.log(res.data);
     setData(res.data)
+    setError('')
   })
   .catch(err => {
-
+    console.log(err);
+    setError(err.message)
   })
+  }
+
+  const findLocation = (e) => {
+    e.preventDefault();
+    getData(search)
   }
 
   return (
     <>
       {data? (
         <div className={styles.page}>
+          {error? (
+            <div className={styles.error}>
+              {error}
+            </div>
+            ) : (<></>)}
         <div className={styles.page__image}>
           <img src='https://images.pexels.com/photos/933054/pexels-photo-933054.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' alt="" />
           <div className={styles.page__mask}></div>
@@ -44,12 +60,33 @@ const App = () => {
              <div className={styles.page__right__content}>
               <h2>{data.main.temp}&deg;</h2>
                 <div className={styles.page__data}>
-                  <h3>Pavlodar</h3>
+                  <h3>{data.name}</h3>
                   <h4>{fullTime}</h4>
                 </div>
              </div>
+             <div className={styles.page_right_card}>
+              
+             </div>
             </div>
             <div className={styles.page__left}>
+              <form className={styles.line}>
+                <select className={styles.translator}>
+                  <option value="en">En</option>
+                  <option value="kz">Kz</option>
+                  <option value="ru">Ru</option>
+                </select>
+                <input 
+                  type="text" 
+                  className={styles.input}
+                  value={search}
+                  onChange={e => setSearch(e.target.value)}
+                 />
+                 <button 
+                 onClick={findLocation}
+                  className={styles.btn}
+                  type='submit'
+                 ><FaSearch className ={styles.btn__icon} /></button>
+              </form>
                 <div className={styles.line}>
                   <h4>Feels like</h4>
                   <span>{data.main.feels_like}&deg;C</span>
